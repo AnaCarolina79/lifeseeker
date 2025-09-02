1b4d6dce9cde4f1bb824e7b8c22aed85f801ffe1
AnaCarolina079 <anacarolinamoreiraluna@gmail.com>
2025-09-02 17:31:23 -0300

    adicionando coisas na parte principal

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
-import { Text, View, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList, StyleSheet, Dimensions } from "react-native";
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_500Medium } from "@expo-google-fonts/inter";
 
 export default function Index() {
+  const { width } = Dimensions.get("window");
+const images = [
+  require("../assets/images/insetos.jpg"),
+  require("../assets/images/insetos.jpg"),
+  require("../assets/images/insetos.jpg"),
+  require("../assets/images/insetos.jpg"),
+  require("../assets/images/insetos.jpg"),
+  require("../assets/images/insetos.jpg"),
+];
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
@@ -17,33 +26,45 @@ export default function Index() {
     >
       
       
-      <Image source={require("../assets/images/insect.jpg")} style={{
-          resizeMode: "contain",
-          width:"110%",
-          height:"70%",
-        margin:-60,
-      }}/>
+   
       <View style={{
         backgroundColor:"#D9D9D9",
         width:"100%",
-        height:"45%",
+        height:"90%",
         borderTopLeftRadius:40,
         borderTopRightRadius:40,
+        padding:20,
+       
 
 
       }}>
+        <View style={{ 
+          justifyContent:"space-between",
+          flexDirection:"row",
+          }}>
+
+           <Image source={require("../assets/images/insect.jpg")} style={{
+          resizeMode: "cover",
+          width:140,
+          height:180,
+          borderRadius:30,
+
+      }}/>
         <View style={{
-          padding:20,
+       padding: 20,
           display:"flex",
           alignItems:"center",
-      }}>
+          width:240
+}}>
         
-      <View style={{}}>
+      <View style={{
+      }}>
             <Text  style={{
               color:"#284026",
               fontFamily: "Inter_600SemiBold",
+              marginBottom:5,
             }}>
-              Você tem um pet e quer saber a raça dele?
+              Você tem um inseto e quer descobrir qual é a espécie?
             </Text>
             <Text style={{
               fontFamily: "Inter_400Regular",
@@ -54,21 +75,65 @@ export default function Index() {
                <Text style={styles.buttonText}>
                 Analisar
                 </Text>
-
+                <Image source={require('../assets/images/arrow.png')}/>
           </TouchableOpacity>
-          </View>
-        </View>
+      </View>    
       </View>
+        </View>
+     <View>
+      <View style={{
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        gap: 5,
+        marginTop:35,
+        marginBottom: 5, 
+      }}>
+      <Image source={require('../assets/images/butterfly.png')}/>
+      <Text style={{ fontFamily: "Inter_600SemiBold", fontSize:15,}}>Você Sabia?</Text>
+      </View>
+      <Text>Insetos podem sobreviver sem a cabeça por alguns dias, como algumas formigas e baratas.</Text>
+     </View>
+      <View style={{
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        gap: 5,
+        marginTop:35,
+        marginBottom: 5, 
+      }}>
+      <Image source={require('../assets/images/butterfly.png')}/>
+      <Text style={{ fontFamily: "Inter_600SemiBold", fontSize:15,}}>Histórico</Text>
+      </View>
+       <FlatList
+      data={images}
+      horizontal
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(_, i) => i.toString()}
+      renderItem={({ item }) => (
+        <Image source={item} style={{borderRadius:20, margin: 5, width:150, height: 180, resizeMode: "cover" }} />
+      )}
+    />
+      </View>
+      
     </View>
   );
 }
 const styles = StyleSheet.create({
 button: {
     marginTop: 20,
-    backgroundColor: "#466619",
-    paddingVertical: 10,
-    paddingHorizontal: 25,
+    backgroundColor: "#b96d52ff",
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    width:"70%",
+    display:"flex",
+    alignItems:"center",
+    flexDirection:"row",
+    justifyContent:"center",
+    gap:5,
     borderRadius: 30,
+    boxShadow:'0px 3px 8px rgba(61, 30, 30, 0.27)'
   },
   buttonText: {
     color: "#ffffff",
